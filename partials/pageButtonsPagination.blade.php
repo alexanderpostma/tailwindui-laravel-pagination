2f86371ac1d3d7d1478d4ac88ace3f8aaed7419b
Alexander <alexander@appademic.nl>
2020-07-15 17:42:04 +0200

    fix: pagination small bg fix

--- partials/pageButtonsPagination.blade.php
+++ partials/pageButtonsPagination.blade.php
@@ -49,7 +49,7 @@
                     @endif
                     @foreach($elements as $element)
                         @if (is_string($element))
-                        <span class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-gray-100 text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
+                        <span class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
                             {{ $element }}
                         </span>
                         @endif
